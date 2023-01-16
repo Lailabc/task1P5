3699f62e88c24f20776b47b11c95c0420529c9af
Lailabc <115160711+Lailabc@users.noreply.github.com>
2023-01-16 16:12:45 +0000

    Update sketch.js

--- p5-basicAnim-main/base-p5-project-main/js/sketch.js
+++ p5-basicAnim-main/base-p5-project-main/js/sketch.js
@@ -1,23 +1,14 @@
 
 let t = 0; // time variable
-let purple;
-let blue;
-let lightBlue;
-let green;
-let lightGreen;
-let pink;
-let colors = [purple,blue,lightBlue,green,lightGreen,pink]
+
+let colors = [];
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
-  purple = color(250,0,250);
-blue = color(0,0,255);
-lightBlue = color(0,250,250)
-green = color(0,255,0)
-lightGreen = color(0,255,100)
-pink = color(255,0,150)
+
+colors = [color(250,0,250),color(0,0,255),color(0,250,250),color(0,255,0),color(0,255,100),color(255,0,150)]
 }
 
 function draw() {
@@ -36,13 +27,12 @@ function draw() {
       // each particle moves in a circle
       const myX = x + 10 * cos(4 * PI * t + angle);
       const myY = y + 50 * sin(2 * PI * t + angle);
-      fill(colors);
+      fill(random(colors.length));
 
       ellipse(myX, myY, 50); // draw particle
     }
   }
  
   t = t + 0.01; // update time
+  
 }
-function mousePressed(){}
-   
